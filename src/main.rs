b4db8824072e6fd1f70c8bf5e9da7427d006e05f
notplants <mfowler.email@gmail.com>
2021-04-20 15:48:24 +0200

    Working tbot

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use dotenv;
 use futures::StreamExt;
 use telegram_bot::*;
 use telegram_bot::{Api, Message, SendMessage, MessageKind, UpdateKind};
-mod email;
 mod tele;
 use tele::TStruct;
 use std::result::Result;
@@ -12,62 +11,23 @@ use std::{thread, time};
 
 
 
+
+
+
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    dotenv::dotenv().ok();
+    dotenv::from_path("/etc/peachtbot.conf").ok();
     let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
     let api = Api::new(token);
-
-//      api.send(message.text_reply(format!("+ wgbot is booting up")));
-
-    let imap_username = env::var("IMAP_USERNAME").expect("imap username is not set");
-    let imap_password = env::var("IMAP_PASSWORD").expect("imap password is not set");
     let telegram_log_id: i64 = env::var("TELEGRAM_LOG_ID").expect("TELEGRAM_LOG_ID is not set").parse().unwrap();
 
     let t:  TStruct = TStruct::new(api, telegram_log_id);
     t.test();
-    t.log("++ wgbot is online").await?;
+    t.log("++ peach-tbot is online").await?;
 
-    loop {
-       println!("++ fetching new emails");
-       let email = email::fetch_inbox_top(&imap_username, &imap_password, &t);
-        match email {
-            Ok(v) => {
-                println!("positive result");
-                match v {
-                    Some(x) => {
-                        let log_msg = format!("++ new listing: {}", x);
-                        t.log(&log_msg).await?;
-                    }
-                    None => println!("None returned")
-                }
-            },
-            Err(e) => println!("error: {:?}", e),
-        }
-        let wait_time = time::Duration::from_millis(5000);
-        thread::sleep(wait_time);
-    }
+    t.log_ip().await?;
+    t.poll().await?;
 
-//
-//    // Fetch new updates via long poll method
-//    let mut stream = api.stream();
-//    while let Some(update) = stream.next().await {
-//        // If the received update contains a new message...
-//        let update = update?;
-//        if let UpdateKind::Message(message) = update.kind {
-//            if let MessageKind::Text { ref data, .. } = message.kind {
-//                // Print received text message to stdout.
-//                println!("<{}>: {}", &message.from.first_name, data);
-//                println!("user id: {}", &message.from.id);
-//
-//                // Answer message with "Hi".
-//                api.send(message.text_reply(format!(
-//                    "Hi, {}! You just wrote '{}'",
-//                    &message.from.first_name, data
-//                )))
-//                .await?;
-//            }
-//        }
-//    }
     Ok(())
 }
--- src/tele.rs
+++ src/tele.rs
@@ -1,6 +1,11 @@
 use telegram_bot::*;
 use telegram_bot::{Api, Message, SendMessage, MessageKind, UpdateKind};
 use std::result::Result;
+use futures::StreamExt;
+extern crate pnet;
+use pnet::datalink;
+use pnet::ipnetwork;
+use std::process::Command;
 
 
 pub struct TStruct {
@@ -25,5 +30,70 @@ impl TStruct {
         let s:SendMessage = SendMessage::new(&self.chat_id, msg);
         self.api.send(s).await?;
         Ok(())
+    }
+
+    pub async fn poll(&self) -> Result<(), telegram_bot::Error>{
+        // Fetch new updates via long poll method
+        let mut stream = self.api.stream();
+        while let Some(update) = stream.next().await {
+            // If the received update contains a new message...
+            let update = update?;
+            if let UpdateKind::Message(message) = update.kind {
+                if let MessageKind::Text { ref data, .. } = message.kind {
+                    // Print received text message to stdout.
+                    println!("<{}>: {}", &message.from.first_name, data);
+                    println!("user id: {}", &message.from.id);
+                    let command = data.as_str();
+                    if command == "ip" {
+                        self.log_ip().await?;
+                    } else if command == "public" {
+                        self.log("++ attempting to get public ip").await?;
+                        let public_ip =  self.public_ip().await;
+                        self.log(&format!("++ public ip: {}", public_ip)).await?
+                    }
+                    else {
+                        // unknown command
+                        self.api.send(message.text_reply(format!(
+                            "unknown command '{}'", data
+                        )))
+                        .await?;
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub async fn log_ip(&self) -> Result<(), telegram_bot::Error> {
+        let ifaces= ["eth0", "wlan0"];
+        self.log("++ attemping to get ip address").await?;
+        for iface in datalink::interfaces() {
+            if ifaces.contains(&iface.name.as_str()) {
+                println!("{}: {:?}", &iface.name, iface);
+                // loop through ips
+                for ip in iface.ips {
+                    match ip {
+                        ipnetwork::IpNetwork::V4(ipNetwork) => {
+                            println!("ip: {:?}", ipNetwork.ip());
+                            let log_msg = &format!("++ {} ip: {}", &iface.name, ipNetwork.ip());
+                            println!("{}", log_msg);
+                            self.log(log_msg).await?;
+                        }
+                        _ => {
+                            println!("no match");
+                        }
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub async fn public_ip(&self) -> String {
+        let output = Command::new("/usr/bin/curl")
+            .arg("ifconfig.me")
+            .output().expect("failed to get public IP");
+        let command_output = std::str::from_utf8(&output.stdout).expect("Incorrect format");
+        command_output.to_string()
     }
 }
